14caa49cf58a236bf430841964e2f8fa7852f5c8
jinyanlin <jinyanlin@renrenfenqi.com>
2016-08-06 15:26:42 +0800

    Update

--- app/Console/Commands/IotCommand.php
+++ app/Console/Commands/IotCommand.php
@@ -13,7 +13,7 @@ class IotCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'iot';
+    protected $signature = 'iot {action=test}';
 
     /**
      * The console command description.
@@ -31,15 +31,18 @@ class IotCommand extends Command
     
     
     public function initClient(){
-        $accessKeyId = "nL5Y7fL9P7RXUZ5J";
-        $accessSecret = "saBQK7zYCkWXBi7vV7YCI8Fl7kc5i2";
-        $iClientProfile = \DefaultProfile::getProfile("cn-hangzhou", $accessKeyId, $accessSecret);
-        $this->client = new \DefaultAcsClient($iClientProfile);
+        if(null == $this->client){
+            $accessKeyId = "nL5Y7fL9P7RXUZ5J";
+            $accessSecret = "saBQK7zYCkWXBi7vV7YCI8Fl7kc5i2";
+            $iClientProfile = \DefaultProfile::getProfile("cn-hangzhou", $accessKeyId, $accessSecret);
+            $this->client = new \DefaultAcsClient($iClientProfile);
+        }
     }
     
     
-    public function testSub(){
-
+    public function subAction(){
+        $this->initClient();
+        
         $request = new Iot\SubRequest();
         $request->setProductKey(23344127);
         $request->setSubCallback("http://api.xb.guozhongbao.com/mock/consumer");//当topic有消息时候，接受消息的地址，参考服务器回调
@@ -47,12 +50,12 @@ class IotCommand extends Command
         $response = $this->client->getAcsResponse($request);
         print_r("\r\n");
         print_r($response);
-        
     }
     
     
-    public function testPub(){
-    
+    public function pubAction(){
+        $this->initClient();
+        
         $request = new Iot\PubRequest();
         $request->setProductKey(23344127);
         $request->setMessageContent("aGVsbG93b3JsZA==");// Hello world base64 String.
@@ -61,10 +64,23 @@ class IotCommand extends Command
         
         print_r("\r\n");
         print_r($response);
-        
-        
     }
     
+    
+    public function testAction(){
+
+        // sign = 061397c0ec57664173a81c0345dbbba9
+        $str = '{\"message\":\"aGVsbG93b3JsZA==\",\"topic\":\"/23344127/#\",\"sign\":\"061397c0ec57664173a81c0345dbbba9\",\"messageId\":4,\"appKey\":\"23344127\"}"}';
+        dump(stripslashes($str));
+        
+        $this->comment(md5('23344127e3RlbToyMH0=1be73f483da7bcd875e0496d57603a99'));
+    }
+    
+    public function fireAction(){
+    
+    
+        $this->comment(__FUNCTION__);
+    }
     
     /**
      * Execute the console command.
@@ -73,23 +89,12 @@ class IotCommand extends Command
      */
     public function handle()
     {
-        // sign = 061397c0ec57664173a81c0345dbbba9
-        $str = '{\"message\":\"aGVsbG93b3JsZA==\",\"topic\":\"/23344127/#\",\"sign\":\"061397c0ec57664173a81c0345dbbba9\",\"messageId\":4,\"appKey\":\"23344127\"}"}';
-        dump(stripslashes($str));
-        
-        $this->comment(md5('23344127e3RlbToyMH0=1be73f483da7bcd875e0496d57603a99'));
-        
-        
-        
-        exit;
-        $this->initClient();
-        
-        $this->testPub();
-        
-        
-        
-        // 23344127 aGVsbG93b3JsZA== 1be73f483da7bcd875e0496d57603a99
-        //sign= md5_32(productKey+(message)+productSecret)
-//         $this->comment(PHP_EOL.Inspiring::quote().PHP_EOL);
+        $action = $this->argument('action');
+        $funcName = strtolower($action).'Action';
+        if(method_exists($this, $funcName)){
+            call_user_func([$this,$funcName]);
+        }else{
+            $this->error(PHP_EOL. 'No Action Found');
+        }
     }
 }
