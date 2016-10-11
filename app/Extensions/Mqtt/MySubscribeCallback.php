beb9964da6d7bff0a8ae34329d103f4bdc4d814f
jinyanlin <jinyanlin@renrenfenqi.com>
2016-10-11 16:29:20 +0800

    Update Simple Proto

--- app/Extensions/Mqtt/MySubscribeCallback.php
+++ app/Extensions/Mqtt/MySubscribeCallback.php
@@ -23,9 +23,27 @@ class MySubscribeCallback extends MessageHandler
         echo $output.PHP_EOL;
     }
     
+    public function decodeProtoData($msg,$class,$headerLength = 0,$aes = false){
+        $packed = $msg;
+        if($headerLength > 0 ){
+            $packed = substr($msg, $headerLength);
+            dump("Received Body Bytes");
+            $this->dumpByte($packed);
+        }
+        if($aes){
+            $decryptedPack = $this->aesDecrypt($packed);
+            dump("aesDecrypt Body Bytes");
+            $this->dumpByte($decryptedPack);
+            $obj = $class->parseFromString($decryptedPack);
+            return $obj;
+        }else{
+            $obj = $class->parseFromString($packed);
+            return $obj;
+        }
+    }
+    
     public function publish(MQTT $mqtt, PUBLISH $publish_object)
     {
-        
         printf(
             "\e[32mI got a message\e[0m:(msgid=%d, QoS=%d, dup=%d, topic=%s) \e[32m%s\e[0m\n",
             $publish_object->getMsgID(),
@@ -34,40 +52,10 @@ class MySubscribeCallback extends MessageHandler
             $publish_object->getTopic(),
             $publish_object->getMessage()
         );
-        
-        
         $msg = $publish_object->getMessage();
-        
-
-        $headerLength = 8 ;
-        $headerBytes = [];
-        $packed = '';
-//         $bytes[] = ord($msg[$i]);
-        for($i = 0 ; $i < strlen($msg) ; $i ++){
-            if($i < $headerLength){
-                $headerBytes[] = ord($msg[$i]);
-            }else{
-//                 \Proto2\Scentrealm::class
-            }
-        }
-        dump("Received Bytes");
-        $this->dumpByte($msg);
-        if($headerBytes[0] == 0xfe){
-            
-            $packed = substr($msg, $headerLength);
-            dump("Received Body Bytes");
-            $this->dumpByte($packed);
-            
-            $decryptedPack = $this->aesDecrypt($packed);
-            $decryptedPack = chr(0x0a).$decryptedPack;
-            dump("aesDecrypt Body Bytes");
-            $this->dumpByte($decryptedPack);
-            
-            $AuthRequest = new \Proto2\Scentrealm\AuthRequest();
-            $obj = $AuthRequest->parseFromString($decryptedPack);
-            dump($obj);
-            $AuthRequest->dump();
-        }
-        
+        $class = new \Proto2\Scentrealm\Simple\PlaySmell();
+        $ret = $this->decodeProtoData($msg, $class);
+        dump($ret);
+        $class->dump();
     }
 }
