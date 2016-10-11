e38517e6e5d3c1b33e1dd487e0bc2f922017093c
jinyanlin <jinyanlin@renrenfenqi.com>
2016-10-11 16:38:14 +0800

    Update

--- app/Extensions/Mqtt/MySubscribeCallback.php
+++ app/Extensions/Mqtt/MySubscribeCallback.php
@@ -44,6 +44,7 @@ class MySubscribeCallback extends MessageHandler
     
     public function publish(MQTT $mqtt, PUBLISH $publish_object)
     {
+        
         printf(
             "\e[32mI got a message\e[0m:(msgid=%d, QoS=%d, dup=%d, topic=%s) \e[32m%s\e[0m\n",
             $publish_object->getMsgID(),
@@ -52,10 +53,30 @@ class MySubscribeCallback extends MessageHandler
             $publish_object->getTopic(),
             $publish_object->getMessage()
         );
+        
+        
         $msg = $publish_object->getMessage();
-        $class = new \Proto2\Scentrealm\Simple\PlaySmell();
-        $ret = $this->decodeProtoData($msg, $class);
-        dump($ret);
-        $class->dump();
+        
+
+        $headerLength = 8 ;
+        $headerBytes = [];
+        $packed = '';
+//         $bytes[] = ord($msg[$i]);
+        for($i = 0 ; $i < strlen($msg) ; $i ++){
+            if($i < $headerLength){
+                $headerBytes[] = ord($msg[$i]);
+            }else{
+//                 \Proto2\Scentrealm::class
+            }
+        }
+        dump("Received Bytes");
+        $this->dumpByte($msg);
+        if($headerBytes[0] == 0xfe){
+            $class = new \Proto2\Scentrealm\Simple\PlaySmell();
+            $ret = $this->decodeProtoData($msg, $class,$headerLength);
+            dump($obj);
+            $class->dump();
+        }
+        
     }
 }
